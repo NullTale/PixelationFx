3f39a2a132bb4c30680fa8e20a2a9e6f9a3b338d
Null Tale <nulltale@gmail.com>
2024-01-14 21:34:32 +0300

    Default grid color clear instead of black

--- Runtime/Pixelation/PixelationVol.cs
+++ Runtime/Pixelation/PixelationVol.cs
@@ -12,7 +12,7 @@ namespace VolFx
         public ClampedFloatParameter         m_Scale     = new ClampedFloatParameter(1, 0, 1f);
         public NoInterpClampedFloatParameter m_Grid      = new NoInterpClampedFloatParameter(1f, 0, 1f);
         public ClampedFloatParameter         m_Roundness = new ClampedFloatParameter(.5f, 0f, 1f);
-        public NoInterpColorParameter        m_Color     = new NoInterpColorParameter(Color.black);
+        public NoInterpColorParameter        m_Color     = new NoInterpColorParameter(Color.clear);
         public ClampedIntParameter           m_Posterize = new ClampedIntParameter(64, 1, 64);
         
         // =======================================================================
